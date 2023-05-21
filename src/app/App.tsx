37dc25c802d5bbec7cb752de9f51eacf077911ad
dmitriy <dmitriy.blinov@softlex.pro>
2023-05-21 15:44:20 +0300

    fix theme for modal

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,11 @@
 import { Suspense } from "react";
-import { useTheme } from "app/providers/ThemeProvider";
 import { AppRouter } from "app/providers/router";
 import { Navbar } from "widgets/Navbar";
 import { Sidebar } from "widgets/Sidebar";
-import { classNames } from "shared/lib/classNames/classNames";
 
 const App = () => {
-  const { theme } = useTheme();
-
   return (
-    <div className={classNames("app", {}, [theme])}>
+    <div className="app">
       <Suspense fallback="">
         <Navbar />
         <div className="content-page">
